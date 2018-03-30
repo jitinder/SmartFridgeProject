882b6a4b9fa11a97fa04d6eb924c287fdead0a2e
venet <zcabvku@ucl.ac.uk>
2018-03-30 16:51:28 +0100

    recipe class

--- app/src/main/java/com/example/android/team49/Recipe.java
+++ app/src/main/java/com/example/android/team49/Recipe.java
@@ -12,43 +12,80 @@ import java.util.List;
 public class Recipe {
     String recipe;
     ArrayList<String> ingredients = new ArrayList<>();
+    ArrayList<String> course = new ArrayList<>();
+    double[] flavours = new double[6];
     String imageUrl;
-    String rating;
+    int rating;
     String source;
     String id;
 
-    public Recipe() {}
+    String cuisine;
 
-    public Recipe(String recipe, ArrayList<String> ingredients, String imageUrl, String rating, String source, String id) {
+    public Recipe(String recipe, ArrayList<String> ingredients, ArrayList<String> course, double[] flavours, String cuisine,
+                  String imageUrl, int rating,String source, String id) {
         this.recipe = recipe;
         this.ingredients = ingredients;
         this.imageUrl = imageUrl;
         this.rating = rating;
         this.source = source;
         this.id = id;
+        this.course = course;
+        this.flavours = flavours;
+        this.cuisine = cuisine;
     }
 
-    public String getRecipeName() {
+    public String getRecipe() {
         return recipe;
     }
 
-    public List<String> getIngredients() {
-        return ingredients;
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
     }
 
-    public String getImageUrl() {
-        return  imageUrl;
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
     }
 
-    public String getRating() {
-        return rating;
+    public ArrayList<String> getCourse() {
+        return course;
     }
 
-    public String getSource() {
-        return source;
+    public void setCourse(ArrayList<String> course) {
+        this.course = course;
     }
 
-    public String getId() { return id; }
+    public double[] getFlavours() {
+        return flavours;
+    }
+
+    public void setFlavours(double[] flavours) {
+        this.flavours = flavours;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
 }
 
 
